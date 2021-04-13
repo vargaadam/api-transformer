2ad62c3069e646a2fb0c366f3e90baea15eb47fc
Varga Adam <vargaad95@gmail.com>
2021-04-13 18:55:10 +0200

    feat(Sport): add SportService

---
+++ src/modules/sport/service.test.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+import SportService from './service';
+
+let sportService: SportService;
+
+describe('SportService', () => {
+  beforeEach(() => {
+    sportService = new SportService();
+  });
+});
