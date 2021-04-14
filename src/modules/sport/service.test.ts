2e8480f9e8f9b60fa35f9135cf2c8267d49ad1a5
Varga Adam <vargaad95@gmail.com>
2021-04-14 22:20:32 +0200

    feat(Sport): add getAllSports method to SportService

--- src/modules/sport/service.test.ts
+++ src/modules/sport/service.test.ts
@@ -1,11 +1,50 @@
 import { expect } from 'chai';
+import sinon, { SinonStubbedInstance } from 'sinon';
+
+import EventsApi from '../../api/events-api';
 
 import SportService from './service';
 
+let mockedEventsApi: SinonStubbedInstance<EventsApi>;
 let sportService: SportService;
+
+const rawEventsResult = [
+  {
+    id: 1,
+    desc: 'Football',
+    comp: []
+  },
+  {
+    id: 2,
+    desc: 'Hockey',
+    comp: []
+  }
+];
 
 describe('SportService', () => {
   beforeEach(() => {
-    sportService = new SportService();
+    mockedEventsApi = sinon.createStubInstance(EventsApi);
+    sportService = new SportService(mockedEventsApi);
+  });
+
+  describe('#getAllSports', () => {
+    it('should return with sports without the competitions', async () => {
+      const expectedResult = [
+        {
+          id: 1,
+          desc: 'Football'
+        },
+        {
+          id: 2,
+          desc: 'Hockey'
+        }
+      ];
+
+      mockedEventsApi.getRawEvents.resolves(rawEventsResult);
+
+      const res = await sportService.getAllSports();
+
+      expect(res).to.eql(expectedResult);
+    });
   });
 });
--- src/modules/sport/service.ts
+++ src/modules/sport/service.ts
@@ -7,4 +7,14 @@ export default class SportService {
   constructor(eventsApi: EventsApi) {
     this.eventsApi = eventsApi;
   }
+
+  async getAllSports(): Promise<ISport[]> {
+    const rawEvents = await this.eventsApi.getRawEvents();
+
+    return rawEvents.map((rawEvent) => {
+      const { comp, ...sport } = rawEvent;
+
+      return sport;
+    });
+  }
 }
