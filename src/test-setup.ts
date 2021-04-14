f49b856af937898863d761a7f2ebff49f667238d
Varga Adam <vargaad95@gmail.com>
2021-04-15 00:42:34 +0200

    deps: add more testing packages

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -2,8 +2,10 @@ import sinon from 'sinon';
 import chai from 'chai';
 
 import sinonChai from 'sinon-chai';
+import chaiAsPromised from 'chai-as-promised';
 
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
 
 afterEach(() => {
   sinon.restore();
