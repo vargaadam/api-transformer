fa2002178cdc7ea4492adfe05bc3815bdadf80e6
Varga Adam <vargaad95@gmail.com>
2021-04-14 22:04:09 +0200

    refactor(EventsApi): use class instead of function

--- src/api/events-api.ts
+++ src/api/events-api.ts
@@ -1,33 +1,37 @@
-import https from 'https';
+import https, { RequestOptions } from 'https';
 import { HttpException } from '../exceptions';
 
-const BASE_URL = 'partners.betvictor.mobi';
+export default class EventsApi {
+  options: RequestOptions;
 
-export const getEvents = async () => {
-  const options = {
-    hostname: BASE_URL,
-    port: 443,
-    path: '/en-gb/in-play/1/events',
-    method: 'GET'
-  };
+  constructor(baseUrl) {
+    this.options = {
+      hostname: baseUrl,
+      port: 443,
+      path: '/en-gb/in-play/1/events',
+      method: 'GET'
+    };
+  }
 
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      if (res.statusCode < 200 || res.statusCode >= 300) {
-        return reject(new HttpException(res.statusCode, res.statusMessage));
-      }
+  getRowEvents(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const req = https.request(this.options, (res) => {
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+          return reject(new HttpException(res.statusCode, res.statusMessage));
+        }
 
-      const data = [];
+        const data = [];
 
-      res.on('data', (d) => {
-        data.push(d);
+        res.on('data', (d) => {
+          data.push(d);
+        });
+
+        res.on('end', () => resolve(JSON.parse(Buffer.concat(data).toString())));
       });
 
-      res.on('end', () => resolve(JSON.parse(Buffer.concat(data).toString())));
+      req.on('error', reject);
+
+      req.end();
     });
-
-    req.on('error', reject);
-
-    req.end();
-  });
-};
+  }
+}
