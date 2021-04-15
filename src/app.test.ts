79e74ab2faf522ca713230594b3484bb62c90cec
Varga Adam <vargaad95@gmail.com>
2021-04-15 11:42:53 +0200

    test(App): mock the eventsApi and extend the sports test

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,22 +1,72 @@
-import { Server } from 'node:http';
+import App from './app';
+import { Server } from 'http';
+import sinon, { SinonStub } from 'sinon';
 import request from 'supertest';
 
-import App from './app';
+import EventsApi from './api/events-api';
+
 import { BaseModule } from './modules';
 import SportModule from './modules/sport';
 
 let app: App<BaseModule>;
 let server: Server;
 
+let mockedGetRawEvents: SinonStub;
+
+const rawEventsResult = {
+  status: {},
+  result: {
+    total_number_of_events: 2,
+    sports: [
+      {
+        id: 1,
+        desc: 'Football',
+        comp: []
+      },
+      {
+        id: 2,
+        desc: 'Hockey',
+        comp: []
+      }
+    ]
+  }
+};
+
 describe('E2E tests', () => {
   before(() => {
+    mockedGetRawEvents = sinon.stub(EventsApi.prototype, 'getRawEvents');
+
     app = new App([SportModule]);
     server = app.listen();
   });
 
   describe('#/sports', () => {
-    it('GET', (done) => {
-      request(server).get('/sports').expect(200, done);
+    describe('GET', () => {
+      it('should return all the sport with status 200', (done) => {
+        mockedGetRawEvents.resolves(rawEventsResult);
+
+        request(server)
+          .get('/sports')
+          .expect(
+            200,
+            {
+              result: {
+                total_number_of_events: 2,
+                sports: [
+                  {
+                    id: 1,
+                    desc: 'Football'
+                  },
+                  {
+                    id: 2,
+                    desc: 'Hockey'
+                  }
+                ]
+              }
+            },
+            done
+          );
+      });
     });
   });
 
